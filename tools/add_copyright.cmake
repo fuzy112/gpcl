cc9e6238b000074f937307bae2851b9a97fbd5be
Chingyat <tsingyat@outlook.com>
2022-01-05 20:18:47 +0800

    add_copyright.cmake: change to 2022

--- tools/add_copyright.cmake
+++ tools/add_copyright.cmake
@@ -9,7 +9,7 @@ function(generate_copyright_notation_for_file filename OUT_VAR)
 // ${filename}
 // ${lines}
 //
-// Copyright (c) 2021 Zhengyi Fu (tsingyat at outlook dot com)
+// Copyright (c) 2022 Zhengyi Fu (tsingyat at outlook dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
