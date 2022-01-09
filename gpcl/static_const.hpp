8f4241bef02392dbe5cc91708585902b4eec60fd
Chingyat <tsingyat@outlook.com>
2022-01-09 07:31:26 +0000

    add static_const variable template

---
+++ gpcl/static_const.hpp
@@ -0,0 +1,13 @@
+#ifndef GPCL_STATIC_CONST_HPP
+#define GPCL_STATIC_CONST_HPP
+
+#include <gpcl/detail/config.hpp>
+
+namespace gpcl {
+
+template <typename T>
+constexpr T static_const{};
+
+} // namespace gpcl
+
+#endif // GPCL_STATIC_CONST_HPP
