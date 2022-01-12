4d5a08ce55a9e80ef0e7bcd3768e785b27090449
Chingyat <tsingyat@outlook.com>
2022-01-12 20:57:37 +0800

    Revert "shared_ptr: refactor ref_count_ptr class hierarchy"
    This reverts commit ea31129b0fd26af06203fd2e755dd697c98d57c7.

--- gpcl/detail/ref_count.hpp
+++ gpcl/detail/ref_count.hpp
@@ -16,7 +16,6 @@
 #include <gpcl/detail/ref_count_base.hpp>
 #include <gpcl/detail/type_traits.hpp>
 #include <gpcl/detail/utility.hpp>
-#include <gpcl/noncopyable.hpp>
 #include <gpcl/unique_ptr.hpp>
 
 #include <memory>
@@ -25,14 +24,12 @@
 namespace gpcl {
 namespace detail {
 
-template <typename Ptr, typename Deleter, typename Allocator>
-class ref_count : public ref_count_base,
-                  private compressed_pair<Ptr, Deleter>,
-                  private Allocator
+template <typename Derived, typename Allocator>
+class ref_count : public ref_count_base, public Allocator
 {
 private:
-  using rebind_allocator = typename std::allocator_traits<
-      Allocator>::template rebind_alloc<ref_count>;
+  using rebind_allocator =
+      typename std::allocator_traits<Allocator>::template rebind_alloc<Derived>;
   using rebind_allocator_traits = std::allocator_traits<rebind_allocator>;
 
   /// destroy the ref_count itself then deallocate the memory.
@@ -41,15 +38,17 @@ private:
     GPCL_ASSERT_CONST(this->use_count() == 0);
     GPCL_ASSERT_CONST(this->weak_count() == 0);
     rebind_allocator alloc{*this};
-    this->~ref_count();
-    rebind_allocator_traits::deallocate(alloc, this, 1);
+    auto p = static_cast<Derived *>(this);
+    p->~Derived();
+    rebind_allocator_traits::deallocate(alloc, p, 1);
   }
 
+protected:
   /// operation function.
   static void *do_operate(ref_count_base *self,
                           ref_count_operation_t op) noexcept
   {
-    auto s = static_cast<ref_count *>(self);
+    auto s = static_cast<Derived *>(self);
     switch (op)
     {
     case delete_control_block:
@@ -75,13 +74,23 @@ private:
     return nullptr;
   }
 
+  /// Constructor.
+  /// @param op_func the operation function, normally &ref_count::do_operate.
+  /// @param a allocator
+  template <typename... Args>
+  explicit ref_count(operation_func_t op_func, Allocator a)
+      : ref_count_base{op_func},
+        Allocator(a)
+  {
+  }
+
 public:
   //// Create a ref_count.
   /// @param allocator allocator to allocate memory for the created ref_count.
   /// @param args arguments passed to the constructor of Manager.
   template <typename... Args>
-  [[nodiscard]] static ref_count *create(const Allocator &allocator,
-                                         Args &&...args)
+  [[nodiscard]] static Derived *create(const Allocator &allocator,
+                                       Args &&...args)
   {
     rebind_allocator alloc{allocator};
 
@@ -89,7 +98,7 @@ public:
         rebind_allocator_traits::allocate(alloc, 1), allocator_delete{alloc});
 
     auto r = ::new (addr.get())
-        ref_count(&do_operate, allocator, std::forward<Args>(args)...);
+        Derived(&do_operate, allocator, std::forward<Args>(args)...);
     (void)addr.release();
     return r;
   }
@@ -102,23 +111,33 @@ private:
     void operator()(void *p) noexcept
     {
       rebind_allocator_traits::deallocate(alloc_,
-                                          reinterpret_cast<ref_count *>(p), 1);
+                                          reinterpret_cast<Derived *>(p), 1);
     }
   };
+};
+
+template <typename Ptr, typename Deleter, typename Allocator>
+class ref_count_ptr
+    : public ref_count<ref_count_ptr<Ptr, Deleter, Allocator>, Allocator>
+{
+  using base_type =
+      ref_count<ref_count_ptr<Ptr, Deleter, Allocator>, Allocator>;
+
+  compressed_pair<Ptr, Deleter> p_;
 
 public:
   /// Delete the managed object.
   inline void delete_managed_object() noexcept
   {
-    GPCL_ASSERT_CONST(this->use_count() == 0);
-    if (this->first())
+    GPCL_ASSERT_CONST(base_type::use_count() == 0);
+    if (p_.first())
     {
-      this->second()(this->first());
-      this->first() = Ptr();
+      p_.second()(p_.first());
+      p_.first() = Ptr();
     }
   }
 
-  inline Deleter *get_deleter() noexcept { return &this->second(); }
+  inline Deleter *get_deleter() noexcept { return &p_.second(); }
 
 #if !defined GPCL_NO_RTTI
   static inline std::type_info *get_deleter_type_info() noexcept
@@ -128,17 +147,17 @@ public:
 #endif
 
 public:
-  ref_count(ref_count_base::operation_func_t op_func, Allocator a, Ptr p,
-            Deleter d)
+  explicit inline ref_count_ptr(ref_count_base::operation_func_t op_func,
+                                Allocator a, Ptr p, Deleter d)
 #if defined GPCL_NO_EXCEPTIONS
-      : ref_count_base(op_func),
-        compressed_pair<Ptr, Deleter>(p, std::move(d)),
-        Allocator(a)
+      : base_type(op_func, a),
+        p_(p, std::move(d))
   {
   }
 #else
-  try : ref_count_base(op_func), compressed_pair
-    <Ptr, Deleter>(p, std::move_if_noexcept(d)), Allocator(a) {}
+  try : base_type(op_func, a), p_(p, std::move(d))
+  {
+  }
   catch (...)
   {
     d(p);
@@ -146,11 +165,8 @@ public:
   }
 #endif
 
-  inline ~ref_count() noexcept { delete_managed_object(); }
+  inline ~ref_count_ptr() noexcept { delete_managed_object(); }
 };
-
-template <typename Ptr, typename Deleter, typename Allocator>
-using ref_count_ptr = ref_count<Ptr, Deleter, Allocator>;
 
 } // namespace detail
 } // namespace gpcl
