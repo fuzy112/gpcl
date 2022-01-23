f3687b60b36d293663f7fab71cb8f1c4614a6b64
chingyat <tsingyat@outlook.com>
2022-01-23 20:51:38 +0800

    Fix example/exception

--- examples/exception/main.cpp
+++ examples/exception/main.cpp
@@ -1,7 +1,4 @@
-#include <gpcl/debugstream.hpp>
-#include <gpcl/error.hpp>
-#include <gpcl/exception.hpp>
-#include <gpcl/stacktrace.hpp>
+#include <gpcl.hpp>
 
 #include <cerrno>
 #include <cstdio>
@@ -16,30 +13,11 @@ using stacktrace_errinfo =
 std::string to_string(cerrno_errinfo const &ei)
 {
   std::ostringstream ss;
-  std::string str;
-  str.resize(100);
 
-#if defined(__STDC_LIB_EXT1__)
-  strerror_s(&str[0], str.size(), ei.value());
-#elif defined(GPCL_POSIX)
-  int err;
-  do
-  {
-    err = strerror_r(ei.value(), &str[0], str.size());
-    if (err == EINVAL)
-      str = "Unknown error";
-    if (err == 0)
-      break;
-    str.resize(str.size() * 2);
-  } while (err == ERANGE);
-#else
-  str = std::strerror(ei.value());
-#endif
-  str.resize(strlen(str.c_str()));
 
   ss << "["
      << "cerrno_errinfo"
-     << " ] = { " << ei.value() << ", " << std::quoted(str) << " }";
+     << " ] = { " << ei.value() << ", " << std::quoted(gpcl::strerror(ei.value())) << " }";
   return ss.str();
 }
 
@@ -72,9 +50,9 @@ int main()
     FILE *fp = openFile("non-existing-file.txt");
     std::fclose(fp);
   }
-  GPCL_CATCH(gpcl::exception const &exc)
+  GPCL_CATCH(my_error const &exc)
   {
-    gpcl::cdebug() << "Exception caught: " << exc << std::endl;
+    gpcl::cdebug() << exc << std::endl;
   }
   GPCL_CATCH_END
 }
