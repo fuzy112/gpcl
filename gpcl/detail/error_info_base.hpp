c12104fa84ca8e2c6195f19c73dcdddb809af426
Chingyat <tsingyat@outlook.com>
2022-01-28 21:45:46 +0800

    add license information to source files

--- gpcl/detail/error_info_base.hpp
+++ gpcl/detail/error_info_base.hpp
@@ -1,3 +1,13 @@
+//
+// error_info_base.hpp
+// ~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2022 Zhengyi Fu (tsingyat at outlook dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #ifndef GPCL_DETAIL_ERROR_INFO_BASE_HPP
 #define GPCL_DETAIL_ERROR_INFO_BASE_HPP
 
