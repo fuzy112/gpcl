ed6ad772e5a05035409a0e4ebca9e8f315252805
Chingyat <tsingyat@outlook.com>
2022-02-08 23:57:35 +0000

    Reformat cmake files

--- tools/generate_convenient_header.cmake
+++ tools/generate_convenient_header.cmake
@@ -1,11 +1,12 @@
 set(INCLUDES "")
 
 file(GLOB files "${GPCL_SOURCE_DIR}/gpcl/*.hpp")
-foreach (file ${files})
-    get_filename_component(filename ${file} NAME)
-    if (NOT filename STREQUAL "ext.hpp")
-        string(APPEND INCLUDES "#include <gpcl/${filename}>\n")
-    endif ()
-endforeach (file ${files})
+foreach(file ${files})
+  get_filename_component(filename ${file} NAME)
+  if(NOT filename STREQUAL "ext.hpp")
+    string(APPEND INCLUDES "#include <gpcl/${filename}>\n")
+  endif()
+endforeach(file ${files})
 
-configure_file("${CMAKE_CURRENT_LIST_DIR}/gpcl.hpp.in" "${GPCL_SOURCE_DIR}/gpcl.hpp")
+configure_file("${CMAKE_CURRENT_LIST_DIR}/gpcl.hpp.in"
+               "${GPCL_SOURCE_DIR}/gpcl.hpp")
