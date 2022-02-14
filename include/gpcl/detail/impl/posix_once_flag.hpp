8bf05d2085b49dab65637de50a51dbef068b180a
Chingyat <tsingyat@outlook.com>
2022-02-14 22:55:06 +0800

    fix call_once on linux

--- include/gpcl/detail/impl/posix_once_flag.hpp
+++ include/gpcl/detail/impl/posix_once_flag.hpp
@@ -15,19 +15,10 @@
 #include <gpcl/detail/throw_system_error.hpp>
 #include <gpcl/unique_lock.hpp>
 
-#if defined(GPCL_LINUX)
-#  include <gpcl/detail/futex.hpp>
-#endif
-
 #include <atomic>
 #include <climits>
+#include <csetjmp>
 #include <exception>
-
-#ifdef __GLIBC__
-#  include <gnu/libc-version.h>
-#endif
-
-# include <csetjmp>
 
 #include <pthread.h>
 
@@ -46,13 +37,13 @@ void call_once(detail::posix_once_flag &flag, Callable &&callable,
     GPCL_TRY { callable(std::forward<Args>(args)...); }
     GPCL_CATCH(...)
     {
-#ifndef __FreeBSD__
+#if defined(__GLIBC__)
       exc = std::current_exception();
       std::longjmp(jb, 1);
 #else
       goto cleanup;
       pthread_cleanup_push(NULL, NULL);
-cleanup:
+    cleanup:
       pthread_cleanup_pop(1);
       throw;
 #endif
@@ -60,41 +51,10 @@ cleanup:
     GPCL_CATCH_END
   };
   ::gpcl::detail::posix_once_functor = &func;
-#ifndef __FreeBSD__
-  if (setjmp(jb) != 0)
+
+#if defined(__GLIBC__)
+  if (setjmp(jb) > 0)
   {
-
-#if defined(__GLIBC__) && __GLIBC__ == 2 && __GLIBC_MINOR__ >= 20
-    const int initialization_not_started = PTHREAD_ONCE_INIT;
-
-#  if defined(GPCL_DEBUG)
-    const int val = std::atomic_load_explicit(
-        reinterpret_cast<std::atomic_int *>(&flag.data_),
-        std::memory_order::memory_order_acquire);
-    GPCL_ASSERT(val != initialization_not_started);
-#  endif
-
-    // Do a store_release indicating that initialization is not yet started.
-    std::atomic_store_explicit(reinterpret_cast<std::atomic_int *>(&flag.data_),
-                               initialization_not_started,
-                               std::memory_order_release);
-
-    // Wake up any waiters, if any.
-    int s =
-        detail::futex(&flag.data_, FUTEX_WAKE, INT_MAX, nullptr, nullptr, 0);
-    if (s == -1)
-    {
-      GPCL_UNREACHABLE("futex-FUTEX_WAKE");
-    }
-#elif defined(__CYGWIN__)
-    int s = pthread_mutex_unlock(&flag.data_.mutex);
-    if (s != 0)
-    {
-      GPCL_UNREACHABLE("pthread_mutex_unlock");
-    }
-#else
-    { pthread_cleanup_pop(1); }
-#endif
     std::rethrow_exception(exc);
   }
 #endif
