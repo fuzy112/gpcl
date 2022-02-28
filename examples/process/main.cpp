05b18f1db1f0125ca476051a30e3b748b7a10241
Chingyat <tsingyat@outlook.com>
2022-02-28 22:53:59 +0800

    Example/process: test multiple thread

--- examples/process/main.cpp
+++ examples/process/main.cpp
@@ -3,44 +3,54 @@
 #include <gpcl/process.hpp>
 #include <gpcl/signal.hpp>
 #include <gpcl/stacktrace.hpp>
+#include <gpcl/thread.hpp>
 
 int main(int argc, char **argv)
 {
   (void)argc;
   (void)argv;
 
-  GPCL_TRY
-  {
+  gpcl::dynarray<gpcl::thread> threads;
+
+  for (int i = 0; i < 10; ++i)
+    threads.emplace_back([i] {
+      GPCL_TRY
+      {
 #ifdef GPCL_WINDOWS
-    gpcl::dynarray<std::string> args{"notepad.exe"};
+        gpcl::dynarray<std::string> args{"notepad.exe"};
 #else
-    gpcl::dynarray<std::string> args{"ls"};
+        gpcl::dynarray<std::string> args{"echo", std::to_string(i)};
 #endif
-    gpcl::process proc(args);
+        gpcl::process proc(args);
 
-    if (!proc.try_join_for(gpcl::chrono::seconds(5)))
-    {
-      proc.kill();
-      proc.join();
-    }
+        if (!proc.try_join_for(gpcl::chrono::seconds(0)))
+        {
+          proc.terminate();
+          proc.join();
+        }
 
-    if (proc.exited())
-    {
-      gpcl::cdebug() << "Exit code: " << proc.exit_code() << std::endl;
-    }
-    else if (proc.killed())
-    {
-      gpcl::cdebug() << "Killed by signal: " << gpcl::signal_name(proc.signal())
-                     << std::endl;
-    }
-  }
-  GPCL_CATCH(gpcl::system_error const &e)
-  {
-    gpcl::cdebug() << e.what() << std::endl;
-  }
-  GPCL_AND_CATCH(std::exception const &e)
-  {
-    gpcl::cdebug() << e.what() << std::endl;
-  }
-  GPCL_CATCH_END
+        if (proc.exited())
+        {
+          gpcl::cdebug() << i << " Exit code: " << proc.exit_code()
+                         << std::endl;
+        }
+        else if (proc.killed())
+        {
+          gpcl::cdebug() << i << " Killed by signal: "
+                         << gpcl::signal_name(proc.signal()) << std::endl;
+        }
+      }
+      GPCL_CATCH(gpcl::system_error const &e)
+      {
+        gpcl::cdebug() << e.what() << std::endl;
+      }
+      GPCL_AND_CATCH(std::exception const &e)
+      {
+        gpcl::cdebug() << e.what() << std::endl;
+      }
+      GPCL_CATCH_END
+    });
+
+  for (auto &t : threads)
+    t.join();
 }
