55f23699cb5ef9cecb4e1d49b1c6ae936b15cd99
chingyat <tsingyat@outlook.com>
2022-03-03 22:55:50 +0800

    example/process: refactor

--- examples/process/main.cpp
+++ examples/process/main.cpp
@@ -8,41 +8,12 @@
 
 int main(int argc, char **argv)
 {
-  gpcl::dynarray<gpcl::thread> threads;
+  gpcl::dynarray<gpcl::process> processes;
+  gpcl::dynarray<char const *> args(std::next(argv), std::next(argv, argc));
 
   for (int i = 0; i < 100; ++i)
-    threads.emplace_back([=] {
-      GPCL_TRY
-      {
-        gpcl::dynarray<char const *> args(std::next(argv),
-                                          std::next(argv, argc));
-        gpcl::process proc(args);
+    processes.emplace_back(args);
 
-        if (!proc.try_join_for(gpcl::chrono::seconds(0)))
-        {
-          proc.terminate();
-          proc.join();
-        }
-
-        if (proc.exited())
-        {
-          gpcl::cdebug() << i << " Exit code: " << proc.exit_code()
-                         << std::endl;
-        }
-        else if (proc.killed())
-        {
-          gpcl::cdebug() << i << " Killed by signal: "
-                         << gpcl::signal_name(proc.signal()) << std::endl;
-        }
-      }
-      GPCL_CATCH(std::exception const &e)
-      {
-        gpcl::cdebug() << e.what() << std::endl;
-      }
-      GPCL_AND_CATCH(...) { gpcl::cdebug() << "UNknown exception\n"; }
-      GPCL_CATCH_END
-    });
-
-  for (auto &t : threads)
-    t.join();
+  for (auto &p : processes)
+    p.join();
 }
