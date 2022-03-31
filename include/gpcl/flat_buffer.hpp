305754d239257065a9b384aad8b04594ad1181a1
Zhengyi Fu <tsingyat@outlook.com>
2022-03-31 20:59:05 +0800

    Add flat_buffer

---
+++ include/gpcl/flat_buffer.hpp
@@ -0,0 +1,123 @@
+#ifndef GPCL_FLAT_BUFFER_HPP
+#define GPCL_FLAT_BUFFER_HPP
+
+#include <gpcl/buffer.hpp>
+#include <gpcl/default_allocator.hpp>
+#include <gpcl/detail/config.hpp>
+#include <gpcl/detail/throw_system_error.hpp>
+
+#include <climits>
+
+namespace gpcl {
+
+template <typename Allocator>
+class basic_flat_buffer
+{
+public:
+  static_assert(
+      std::is_same<typename std::allocator_traits<Allocator>::value_type,
+                   char>::value,
+      "");
+
+  basic_flat_buffer(const basic_flat_buffer &) = delete;
+
+  basic_flat_buffer &operator=(const basic_flat_buffer &) = delete;
+
+  explicit basic_flat_buffer(const Allocator &a = Allocator()) noexcept
+      : alloc_(a),
+        start_(),
+        end_(),
+        input_(),
+        output_()
+  {
+  }
+
+  ~basic_flat_buffer()
+  {
+    typedef std::allocator_traits<Allocator> alloc_traits;
+    alloc_traits::deallocate(alloc_, start_, end_ - start_);
+  }
+
+  Allocator get_allocator() const noexcept { return alloc_; }
+
+  std::size_t size() const noexcept
+  {
+    std::ptrdiff_t r = output_ - input_;
+    GPCL_ASSERT(r >= 0);
+    return r;
+  }
+
+  std::size_t max_size() const noexcept { return INT_MAX; }
+
+  std::size_t capacity() const noexcept
+  {
+    std::ptrdiff_t r = end_ - input_;
+    GPCL_ASSERT(r >= 0);
+    return r;
+  }
+
+  const_buffer data() const noexcept { return buffer(input_, size()); }
+
+  mutable_buffer prepare(std::size_t n)
+  {
+    if (size() + n > max_size())
+    {
+      GPCL_THROW(std::length_error("basic_flat_buffer::prepare"));
+    }
+
+    if (end_ - output_ < n)
+    {
+      if (end_ - output_ + (input_ - start_) >= n)
+      {
+        std::memmove(start_, input_, size());
+        std::size_t isize = size();
+        input_ = start_;
+        output_ = input_ + isize;
+      }
+      else
+      {
+        typedef std::allocator_traits<Allocator> alloc_traits;
+        char *newbuf =
+            static_cast<char *>(alloc_traits::allocate(alloc_, size() + n));
+        if (size() != 0)
+          std::memcpy(newbuf, input_, size());
+        alloc_traits::deallocate(alloc_, start_, end_ - start_);
+        std::size_t isize = size();
+        start_ = newbuf;
+        input_ = start_;
+        output_ = input_ + isize;
+        end_ = start_ + isize + n;
+      }
+    }
+
+    return buffer(output_, n);
+  }
+
+  void commit(std::size_t n) noexcept
+  {
+    output_ += n;
+    if (output_ > end_)
+      output_ = end_;
+  }
+
+  void consume(std::size_t n) noexcept
+  {
+    input_ += n;
+    if (input_ > output_)
+      input_ = output_;
+  }
+
+private:
+  Allocator alloc_;
+
+  char *start_;
+  char *end_;
+  char *input_;
+  char *output_;
+};
+
+using flat_buffer = basic_flat_buffer<default_allocator<char>>;
+
+} // namespace gpcl
+
+#endif // GPCL_FLAT_BUFFER_HPP
--- tests/buffers_test.cpp
+++ tests/buffers_test.cpp
@@ -2,19 +2,20 @@
 #include <gpcl/buffers_range_ref.hpp>
 
 #include "doctest.h"
-#include <iostream>
 #include <array>
+#include <iostream>
 
 struct pod
 {
-    char a;
-    unsigned long b;
-    unsigned c;
+  char a;
+  unsigned long b;
+  unsigned c;
 };
 
-TEST_CASE("buffers_range_ref") {
+TEST_CASE("buffers_range_ref")
+{
   pod data;
-  
+
   std::array<gpcl::const_buffer, 3> buffer_sequence{
       gpcl::buffer(&data.a, 1),
       gpcl::buffer(&data.b, sizeof(data.b)),
@@ -23,9 +24,32 @@ TEST_CASE("buffers_range_ref") {
 
   std::size_t total_size = 0;
   auto ref = gpcl::buffers_range_ref(buffer_sequence);
-  for (auto b : ref) {
-      total_size += b.size();
+  for (auto b : ref)
+  {
+    total_size += b.size();
   }
 
   CHECK(total_size == sizeof(data.a) + sizeof(data.b) + sizeof(data.c));
 }
+
+#include <gpcl/flat_buffer.hpp>
+
+TEST_CASE("flat_buffer")
+{
+  gpcl::flat_buffer fb;
+
+  for (int i = 0; i < 100; ++i)
+  {
+    auto b = fb.prepare(11);
+    REQUIRE(b.size() >= 11);
+    std::memcpy(b.data(), "Hello world", 11);
+    fb.commit(6);
+    CHECK(fb.size() == 6);
+    auto b1 = fb.data();
+    CHECK(b1.size() == 6);
+    CHECK(std::string("Hello ") == std::string((const char *)b1.data(), 6));
+    fb.consume(6);
+    CHECK(fb.size() == 0);
+  }
+}
+
