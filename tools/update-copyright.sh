7f2c7411beb91ecbfe0b5eb250da28c9fe70ff8f
Zhengyi Fu <tsingyat@outlook.com>
2023-02-18 16:07:16 +0800

    Add tools/update-copyright.sh

---
+++ tools/update-copyright.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# update-copyright.sh
+# ~~~~~~~~~~~~~~~~~~~
+#
+# Copyright (c) 2020-2023 Zhengyi Fu (tsingyat at outlook dot com)
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#
+
+set -e
+
+username=$(git config user.name)
+year=$(date +%Y)
+git diff --cached --name-only |
+	xargs sed -i "/Copyright (c) [0-9]\\+\\(-[0-9]\\+\\)\\? ${username}/{
+        s/${year}/${year}/; t;
+	s/\\([0-9]\\+\\)\\(-[0-9]\\+\\)\\?/\\1-${year}/;
+	s/${year}-${year}/${year}/;
+	T;
+	H
+	};
+	\${x; s/.// ; x ; T ; q 1 }"
