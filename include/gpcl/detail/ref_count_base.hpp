4378f9f6ba24eae028a1993438a33f0f8e5188c7
Fu Zhengyi <tsingyat@outlook.com>
2023-02-20 23:34:24 +0800

    Implement ref_count_base using gpcl::atomic

--- include/gpcl/detail/ref_count_base.hpp
+++ include/gpcl/detail/ref_count_base.hpp
@@ -11,70 +11,114 @@
 #ifndef GPCL_DETAIL_REF_COUNT_BASE_HPP
 #define GPCL_DETAIL_REF_COUNT_BASE_HPP
 
+#include <gpcl/assert.hpp>
+#include <gpcl/atomic.hpp>
 #include <gpcl/detail/config.hpp>
-
-#if defined __cpp_lib_atomic_is_always_lock_free
-#  include <gpcl/detail/std_ref_count_base.hpp>
-#elif defined _MSC_VER
-#  include <gpcl/detail/msvc_ref_count_base.hpp>
-#elif defined(__GNUC__)
-#  include <gpcl/detail/gcc_ref_count_base.hpp>
-#endif
-
+#include <gpcl/detail/ref_count_operation.hpp>
 
 namespace gpcl {
 namespace detail {
 
-#ifdef GPCL_DOXYGEN
-
-/// Base class of shared_block<>.
-/// This class implements the reference counting functionality.
 class ref_count_base
 {
 public:
   typedef void *(*operation_func_t)(ref_count_base *self,
                                     ref_count_operation) noexcept;
 
-  /// Increment use count.
-  void get() noexcept;
+  // Increment use count.
+  void get() noexcept
+  {
+    GPCL_ASSERT(use_count() > 0);
 
-  /// Decrement use count and if use count reaches zero, destroy the managed
-  /// object. After the managed object is destroyed, the weak count will be
-  /// decremented. If weak count reaches zero, the control block will be
-  /// destroyed.
-  void put() noexcept;
+    use_count_.fetch_add(1, gpcl::memory_order::relaxed);
+  }
 
-  /// Returns the use count.
-  long use_count() const noexcept;
+  // Decrement use count and if use count reaches zero, destroy the managed
+  // object. After the managed object is destroyed, the weak count will be
+  // decremented. If weak count reaches zero, the control block will be
+  // destroyed.
+  void put() noexcept
+  {
+    GPCL_ASSERT(use_count() > 0);
 
-  /// Increment weak count.
-  void weak_get() noexcept;
+    if (use_count_.fetch_sub(1, gpcl::memory_order::acquire) == 1)
+    {
+      operate(ref_count_operation::destroy_managed_object);
 
-  /// Decrement weak count.
-  /// If weak count reaches zero, the control block will be destroyed.
-  void weak_put() noexcept;
+      if (weak_count_.fetch_sub(1, gpcl::memory_order::acquire) == 1)
+      {
+        operate(ref_count_operation::delete_control_block);
+      }
+    }
+  }
 
-  /// Returns the weak count.
-  long weak_count() const noexcept;
+  // Returns the use count.
+  long use_count() const noexcept
+  {
+    return use_count_.load(gpcl::memory_order::relaxed);
+  }
 
-  /// Try increment the use count, fail if use count equals 0.
-  bool lock() noexcept;
+  // Increment weak count.
+  void weak_get() noexcept
+  {
+    GPCL_ASSERT(weak_count() > 0);
 
-  /// Invoke an operation.
-  void operate(ref_count_operation op) noexcept;
+    weak_count_.fetch_add(1, gpcl::memory_order::relaxed);
+  }
+
+  // Decrement weak count.
+  // If weak count reaches zero, the control block will be destroyed.
+  void weak_put() noexcept
+  {
+    GPCL_ASSERT(weak_count() > 0);
+    if (weak_count_.fetch_sub(1, gpcl::memory_order::acquire) == 1)
+    {
+      GPCL_ASSERT(use_count() == 0);
+      operate(ref_count_operation::delete_control_block);
+    }
+  }
+
+  // Returns the weak count.
+  long weak_count() const noexcept
+  {
+    return weak_count_.load(gpcl::memory_order::relaxed);
+  }
+
+  // Try increment the use count, fail if use count equals 0.
+  GPCL_NODISCARD bool lock() noexcept
+  {
+    GPCL_ASSERT(weak_count() > 0);
+    long old_val = use_count();
+
+    while (old_val > 0)
+    {
+      if (use_count_.compare_exchange_weak(old_val, old_val + 1,
+                                           gpcl::memory_order::release))
+        return true;
+    }
+
+    return false;
+  }
+
+  // Invoke an operation.
+  void *operate(ref_count_operation op) noexcept { return op_func_(this, op); }
 
 protected:
-  /// Constructor.
-  explicit ref_count_base(operation_func_t op_func);
-};
+  // Constructor.
+  explicit constexpr ref_count_base(operation_func_t op_func) noexcept
+      : op_func_(op_func)
+  {
+    GPCL_ASSERT(op_func_);
+  }
 
-#elif defined __cpp_lib_atomic_is_always_lock_free
-typedef std_ref_count_base ref_count_base;
-#elif defined _MSC_VER
-typedef msvc_ref_count_base ref_count_base;
-#elif defined(__GNUC__)
-typedef gcc_ref_count_base ref_count_base;
-#endif
+private:
+  // Use function pointer to avoid overhead of virtual functions.
+  operation_func_t op_func_;
+
+  gpcl::atomic<long> use_count_{1};
+
+  gpcl::atomic<long> weak_count_{1};
+};
 
 } // namespace detail
 } // namespace gpcl
