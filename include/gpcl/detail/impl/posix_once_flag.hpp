2285e5a525ab55fe11d8714afda77394c4937964
Zhengyi Fu <tsingyat@outlook.com>
2023-02-24 07:05:04 +0800

    Update copyright

--- include/gpcl/detail/impl/posix_once_flag.hpp
+++ include/gpcl/detail/impl/posix_once_flag.hpp
@@ -2,7 +2,7 @@
 // posix_once_flag.hpp
 // ~~~~~~~~~~~~~~~~~~~
 //
-// Copyright (c) 2021 Zhengyi Fu (tsingyat at outlook dot com)
+// Copyright (c) 2021-2023 Zhengyi Fu (tsingyat at outlook dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
