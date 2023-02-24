d06fcf054b12034ff54b1c819f550f58281b6970
Zhengyi Fu <tsingyat@outlook.com>
2023-02-24 10:00:23 +0800

    Fix typo

--- include/gpcl/basic_syncbuf.hpp
+++ include/gpcl/basic_syncbuf.hpp
@@ -2,7 +2,7 @@
 // basic_syncbuf.hpp
 // ~~~~~~~~~~~~~~~~~
 //
-// Copyright (c) 2021 Zhengyi Fu (tsingyat at outlook dot com)
+// Copyright (c) 2021-2023 Zhengyi Fu (tsingyat at outlook dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -83,7 +83,7 @@ public:
         mutex_(std::move(other).mutex_)
   {
     other.wrapped_ = false;
-    other.flag_.reset();
+    other.flags_.reset();
     other.mutex_ = nullptr;
   }
 
@@ -101,7 +101,7 @@ public:
     mutex_ = std::move(other).mutex_;
 
     other.wrapped_ = nullptr;
-    other.flag_.reset();
+    other.flags_.reset();
     other.mutex_ = nullptr;
 
     return *this;
